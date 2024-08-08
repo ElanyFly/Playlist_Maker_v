8c3ded5eab0a220d1b47d7b67659886eff413046
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2024-08-08 13:07:26 +0300

    trackId, реализовано добавление треков в historyList

--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -1,13 +1,18 @@
 package com.example.playlistmaker
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.playlistmaker.utils.Constants
+import okhttp3.internal.readFieldOrNull
 
 class TrackAdapter(
     private val track: List<Track>
 ) : RecyclerView.Adapter<TrackViewHolder>() {
+
+    private var historyList: List<Track> = emptyList()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
         return TrackViewHolder(view as ViewGroup)
@@ -19,6 +24,27 @@ class TrackAdapter(
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         holder.bind(track[position])
+        holder.itemView.setOnClickListener {
+            addTrackToList(track[position])
+        }
     }
+
+    fun getHistoryList(): List<Track> {
+        return historyList
+    }
+
+    fun addTrackToList(track: Track) {
+        val mutableHistoryList = historyList.toMutableList()
+        mutableHistoryList.add(0, track)
+        historyList = mutableHistoryList.toList()
+        Log.i("addTrack", "historyList - $historyList")
+//        notifyDataSetChanged()
+    }
+
+    fun checkHistoryMaxSize() {}
+
+    fun checkTrackRepeat() {}
+
+
 
 }
