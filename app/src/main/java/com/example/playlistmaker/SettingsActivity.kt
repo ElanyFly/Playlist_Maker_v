51b8b2a38a5d8cefba4430f76240f648fc986f30
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2024-08-12 15:25:17 +0300

    биндинг активити сеттингс

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,28 +1,23 @@
 package com.example.playlistmaker
 
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import android.widget.FrameLayout
-import android.widget.LinearLayout
 import android.widget.Switch
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
+import com.example.playlistmaker.databinding.ActivityMainBinding
+import com.example.playlistmaker.databinding.ActivitySettingsBinding
 
 
 class SettingsActivity : AppCompatActivity() {
 
-    private val themeSwitch by lazy { findViewById<Switch>(R.id.themeSwitch) }
-    private val backButton by lazy { findViewById<FrameLayout>(R.id.settings_back_button) }
-    private val shareButton by lazy { findViewById<LinearLayout>(R.id.settings_share_button) }
-    private val helpdeskButton by lazy { findViewById<LinearLayout>(R.id.settings_helpdesk_button) }
-    private val agreementButton by lazy { findViewById<LinearLayout>(R.id.settings_user_agreement_button) }
-
+    private var _binding: ActivitySettingsBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding for SettingsBinding must not be null")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,20 +28,23 @@ class SettingsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        themeSwitch.isChecked = SharedPreferencesManager.getSwitchState()
-        themeSwitch.setOnCheckedChangeListener { switcher, isChecked ->
+        _binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.settingsThemeSwitch.isChecked = SharedPreferencesManager.getSwitchState()
+        binding.settingsThemeSwitch.setOnCheckedChangeListener { switcher, isChecked ->
             App.switchTheme(isChecked)
         }
 
-        backButton.setOnClickListener {
+        binding.settingsBackButton.setOnClickListener {
             finish()
         }
 
-        shareButton.setOnClickListener {
+        binding.settingsShareButton.setOnClickListener {
             shareLink(getString(R.string.android_developer_course_link))
         }
 
-        helpdeskButton.setOnClickListener {
+        binding.settingsHelpdeskButton.setOnClickListener {
             sendEmail(
                 arrayOf(getString(R.string.helpdesk_email)),
                 getString(R.string.helpdesk_mail_header),
@@ -54,7 +52,7 @@ class SettingsActivity : AppCompatActivity() {
             )
         }
 
-        agreementButton.setOnClickListener {
+        binding.settingsUserAgreementButton.setOnClickListener {
             openAgreement(getString(R.string.settings_agreement_link))
         }
 
