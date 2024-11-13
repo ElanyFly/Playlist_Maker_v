7b66a554d332a4410030bb8c0cfa26e1bcdde152
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2024-11-13 16:53:43 +0300

    back btn. playlist text

--- app/src/main/java/com/example/playlistmaker/media/presentation/MediaActivity.kt
+++ app/src/main/java/com/example/playlistmaker/media/presentation/MediaActivity.kt
@@ -8,7 +8,6 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityMediaBinding
-import com.example.playlistmaker.databinding.ActivitySearchBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -49,6 +48,9 @@ class MediaActivity : AppCompatActivity() {
 
         tabMediator.attach()
 
+        binding.mediaBackButton.setOnClickListener{
+            finish()
+        }
 
 
     }
