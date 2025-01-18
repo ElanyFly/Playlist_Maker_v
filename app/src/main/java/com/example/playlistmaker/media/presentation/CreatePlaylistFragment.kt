db9eaa9536282c922f25f09137604ffb80e6c125
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2025-01-18 21:35:02 +0300

    исправление полей ввода текста

--- app/src/main/java/com/example/playlistmaker/media/presentation/CreatePlaylistFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media/presentation/CreatePlaylistFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentCreatePlaylistBinding
 
 class CreatePlaylistFragment: Fragment() {
@@ -22,6 +25,18 @@ class CreatePlaylistFragment: Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textInputPlaylistEditText.doAfterTextChanged {
+            it.toString()
+
+            binding.btnCreatePlaylist.setBackgroundColor(
+                requireContext().getColor(R.color.tumbler_head)
+            )
+
+        }
+    }
 
 
 
