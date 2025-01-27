a1a0ce039baaff291c497c2df12d8fcda28a39f4
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2025-01-27 18:23:24 +0300

    bottom sheet trans corners

--- app/src/main/java/com/example/playlistmaker/media/presentation/PlaylistBottomSheetFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media/presentation/PlaylistBottomSheetFragment.kt
@@ -1,9 +1,6 @@
 package com.example.playlistmaker.media.presentation
 
 import android.app.Dialog
-import android.content.Context
-import android.content.res.Resources
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -19,41 +16,20 @@ class PlaylistBottomSheetFragment : BottomSheetDialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.playlist_bottom_sheet, container, false)
-        val displayMetrics = resources.displayMetrics
-        val screenHeight = displayMetrics.heightPixels
-        val peekHeight = (screenHeight * 0.7).toInt()
-view.minimumHeight = peekHeight
-        return view
+        return inflater.inflate(R.layout.playlist_bottom_sheet, container, false)
     }
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dialog = super.onCreateDialog(savedInstanceState)
-        val behavior = dialog.
-        behavior.skipCollapsed = true
-        behavior.state = BottomSheetBehavior.STATE_EXPANDED
-        return 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.TransparentBottomSheetDialogTheme)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val bottomSheet = dialog?.findViewById<LinearLayout>(R.id.bottomSheet)
-        val behavior = BottomSheetBehavior.from(bottomSheet!!)
-        behavior.skipCollapsed = true
-        behavior.state = BottomSheetBehavior.STATE_EXPANDED
-
-//        setupBottomSheetHeight()
-    }
-
-    private fun setupBottomSheetHeight() {
 
         val bottomSheet = dialog?.findViewById<LinearLayout>(R.id.bottomSheet)
         val behavior = BottomSheetBehavior.from(bottomSheet!!)
         behavior.skipCollapsed = true
-        val displayMetrics = resources.displayMetrics
-        val screenHeight = displayMetrics.heightPixels
-        val peekHeight = (screenHeight * 0.7).toInt()
-        this.view
-        behavior.peekHeight = peekHeight
         behavior.state = BottomSheetBehavior.STATE_EXPANDED
     }
 
