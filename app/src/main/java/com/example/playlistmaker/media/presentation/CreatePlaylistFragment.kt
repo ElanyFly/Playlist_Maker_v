06d1ad337b3a4040506044f66ee2ec8ebac881fe
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2025-01-29 21:09:23 +0300

    playlist creation exit dialog + Toast

--- app/src/main/java/com/example/playlistmaker/media/presentation/CreatePlaylistFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media/presentation/CreatePlaylistFragment.kt
@@ -8,9 +8,10 @@ import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
@@ -22,8 +23,9 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.io.File
 import java.io.FileOutputStream
 import java.util.UUID
+import android.widget.Toast as Toast
 
-class CreatePlaylistFragment: Fragment() {
+class CreatePlaylistFragment : Fragment() {
 
     private val viewModel: CreatePlaylistViewModel by viewModel()
 
@@ -51,24 +53,37 @@ class CreatePlaylistFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-            if (uri != null) {
-                fileUri = uri
-                binding.loadImage.setImageURI(uri)
-                loadTemporaryImage(uri)
-                Toast.makeText(requireContext(),
-                    getString(R.string.playlist_image_successfully_loaded), Toast.LENGTH_SHORT).show()
-            } else {
-
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            if (binding.loadImage.drawable != null || inputPlaylistName.isNotBlank() || inputPlaylistDescription.isNotBlank()) {
+                showExitDialog()
             }
         }
 
+        val pickMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                if (uri != null) {
+                    fileUri = uri
+                    binding.loadImage.setImageURI(uri)
+                    loadTemporaryImage(uri)
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.playlist_image_successfully_loaded), Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+
+                }
+            }
+
         binding.backArrow.setOnClickListener {
+            if (binding.loadImage.drawable != null || inputPlaylistName.isNotBlank() || inputPlaylistDescription.isNotBlank()) {
+                showExitDialog()
+            } else {
             findNavController().popBackStack()
+            }
         }
 
         binding.loadImage.setOnClickListener {
-           pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
 
         binding.textInputPlaylistEditText.doAfterTextChanged {
@@ -83,13 +98,12 @@ class CreatePlaylistFragment: Fragment() {
                     requireContext().getColor(R.color.grey_123)
                 )
             }
+
         }
 
         binding.textInputDescriptionEditText.doAfterTextChanged {
             inputPlaylistDescription = it.toString()
         }
-
-
 
         binding.btnCreatePlaylist.setOnClickListener {
             fileUri?.let { saveImageToPrivateStorage(it) }
@@ -101,13 +115,38 @@ class CreatePlaylistFragment: Fragment() {
                 playlistTrackAmount = 0
             )
             viewModel.createPlaylist(newPlaylist)
+            Toast.makeText(context,
+                getString(R.string.Playlist_is_created, newPlaylist.playListName), Toast.LENGTH_SHORT).show()
             findNavController().popBackStack()
         }
 
 
     }
+
+    private fun showExitDialog() {
+
+        val alertDialogBuilder = AlertDialog.Builder(requireContext())
+        alertDialogBuilder.setTitle(getString(R.string.playlist_finish_creation))
+        alertDialogBuilder.setMessage(getString(R.string.playlist_finish_message))
+
+        alertDialogBuilder.setPositiveButton(getString(R.string.playlist_finish)) { dialog, with ->
+            findNavController().popBackStack()
+        }
+
+        alertDialogBuilder.setNegativeButton(getString(R.string.playlist_finish_cancel)) { dialog, with ->
+            dialog.dismiss()
+        }
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+
+    }
+
     private fun loadTemporaryImage(uri: Uri) {
-        filePath = File(requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "PlaylistAlbum")
+        filePath = File(
+            requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+            "PlaylistAlbum"
+        )
         if (filePath?.exists() == false) {
             filePath?.mkdirs()
         } else {
