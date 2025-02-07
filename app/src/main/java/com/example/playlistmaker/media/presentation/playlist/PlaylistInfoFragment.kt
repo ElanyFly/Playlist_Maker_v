5104eacec34af42613f3cbd7de25664117479641
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2025-02-07 15:59:39 +0300

    настройка видимости бш

--- app/src/main/java/com/example/playlistmaker/media/presentation/playlist/PlaylistInfoFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media/presentation/playlist/PlaylistInfoFragment.kt
@@ -4,7 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
+import android.widget.LinearLayout
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
@@ -12,6 +13,7 @@ import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentPlaylistInfoBinding
 import com.example.playlistmaker.media.domain.db.model.PlaylistWithTracksModel
 import com.example.playlistmaker.search.domain.models.Track
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -25,6 +27,8 @@ class PlaylistInfoFragment : Fragment() {
         get() = _binding
             ?: throw IllegalStateException("Binding for FragmentPlaylistInfo must not be null")
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
+
     private var playlistId: Int? = null
     private var trackList: List<Track> = emptyList()
 
@@ -32,25 +36,9 @@ class PlaylistInfoFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentPlaylistInfoBinding.inflate(inflater, container, false)
         return binding.root
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val callback = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                val fragment = parentFragmentManager.findFragmentByTag(TrackShowBsFragment.TAG)
-                if (fragment is TrackShowBsFragment) {
-                    fragment.dismiss()
-                    view?.findNavController()?.popBackStack()
-                } else {
-                    view?.findNavController()?.popBackStack()
-                }
-            }
-        }
-        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -61,31 +49,33 @@ class PlaylistInfoFragment : Fragment() {
             return
         }
 
+        val bottomSheet = view.findViewById<LinearLayout>(R.id.bottomSheetTrackShow)
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet!!)
+        val minHeightBs = resources.getDimensionPixelSize(R.dimen.bottom_sheet_peek_height)
+
+
+
         viewModel.getPlaylistWithTracks(playlistId!!)
         viewModel.playlistWithTracks.observe(viewLifecycleOwner) { playlistWithTracks ->
             setPlaylistInfo(playlistWithTracks)
             trackList = playlistWithTracks.playlistTracks
-            if (trackList.isNotEmpty()) {
-                val trackShowBsFragment = TrackShowBsFragment.newInstance(trackList)
-                trackShowBsFragment.show(parentFragmentManager, trackShowBsFragment.tag)
+
+            if (trackList.isNotEmpty()){
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                bottomSheetBehavior.isHideable = false
+                bottomSheet.minimumHeight = minHeightBs
+                bottomSheetBehavior.peekHeight = minHeightBs
+            } else {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                bottomSheet.isVisible = false
             }
+
         }
 
         binding.backArrow.setOnClickListener {
-//            view.findNavController().popBackStack()
-            val fragment = parentFragmentManager.findFragmentByTag(TrackShowBsFragment.TAG)
-            if (fragment is TrackShowBsFragment) {
-                fragment.dismiss()
-                view.findNavController().popBackStack()
-            } else {
-                view.findNavController().popBackStack()
-            }
-        }
+            view.findNavController().popBackStack()
 
-//        if (trackList.isNotEmpty()) {
-//            val trackShowBsFragment = TrackShowBsFragment.newInstance(trackList)
-//            trackShowBsFragment.show(parentFragmentManager, trackShowBsFragment.tag)
-//        }
+        }
 
     }
 
--- app/src/main/java/com/example/playlistmaker/media/presentation/playlist/TrackShowBsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media/presentation/playlist/TrackShowBsFragment.kt
@@ -1,11 +1,9 @@
 package com.example.playlistmaker.media.presentation.playlist
 
-import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import com.example.playlistmaker.R
@@ -15,8 +13,6 @@ import com.example.playlistmaker.media.presentation.playlist_adapter.TrackShowBs
 import com.example.playlistmaker.search.domain.models.Track
 import com.example.playlistmaker.utils.Constants
 import com.example.playlistmaker.utils.navigateToDestination
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
@@ -38,7 +34,7 @@ class TrackShowBsFragment : BottomSheetDialogFragment() {
             return@TrackShowBsAdapter
         }
         moveJob = lifecycleScope.launch {
-//            viewModel.makeAction(SearchAction.AddTrackToHistoryList(track))
+
             AudioPlayerFragment.newInstance(track)
             navigateToDestination(R.id.audioPlayerFragment)
             delay(Constants.CLICK_DEBOUNCE_DELAY)
@@ -56,39 +52,9 @@ class TrackShowBsFragment : BottomSheetDialogFragment() {
         return binding.root
     }
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dialog = super.onCreateDialog(savedInstanceState)
-        dialog.setCanceledOnTouchOutside(false)
-        isCancelable = false
-        dialog.window?.setDimAmount(0.2f)
-
-        dialog.setOnShowListener {
-            val bottomSheetDialog = it as BottomSheetDialog
-            val bottomSheet = bottomSheetDialog.findViewById<LinearLayout>(R.id.bottomSheetTrackShow)
-            val behavior = BottomSheetBehavior.from(bottomSheet!!)
-            behavior.skipCollapsed = true
-            behavior.isHideable = false
-            behavior.state = BottomSheetBehavior.STATE_EXPANDED
-        }
-
-        return dialog
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        setStyle(STYLE_NORMAL, R.style.TransparentBottomSheetDialogTheme)
-    }
-
+    
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-//        val bottomSheet = dialog?.findViewById<LinearLayout>(R.id.bottomSheetTrackShow)
-//        val behavior = BottomSheetBehavior.from(bottomSheet!!)
-//        behavior.skipCollapsed = true
-//        behavior.state = BottomSheetBehavior.STATE_EXPANDED
-//        behavior.isHideable = false
-
-
         trackList = _trackListTemp ?: run {
             view.findNavController().popBackStack()
             return
@@ -98,7 +64,6 @@ class TrackShowBsFragment : BottomSheetDialogFragment() {
     }
 
     companion object {
-        const val TAG = "BottomSheetDialogFragmentTrackShow"
         private var _trackListTemp: List<Track>? = null
 
         fun newInstance(trackList: List<Track>) = TrackShowBsFragment().apply {
