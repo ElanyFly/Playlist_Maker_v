7c4cd5a3817b496d6632bfc6193ab30a9dcad034
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2025-02-07 16:30:42 +0300

    отображение плейлиста

--- app/src/main/java/com/example/playlistmaker/media/presentation/playlist/PlaylistInfoFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media/presentation/playlist/PlaylistInfoFragment.kt
@@ -7,13 +7,22 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.playlistmaker.R
+import com.example.playlistmaker.audio_player.presentation.AudioPlayerFragment
 import com.example.playlistmaker.databinding.FragmentPlaylistInfoBinding
 import com.example.playlistmaker.media.domain.db.model.PlaylistWithTracksModel
+import com.example.playlistmaker.media.presentation.playlist_adapter.TrackShowBsAdapter
 import com.example.playlistmaker.search.domain.models.Track
+import com.example.playlistmaker.utils.Constants
+import com.example.playlistmaker.utils.navigateToDestination
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -29,8 +38,26 @@ class PlaylistInfoFragment : Fragment() {
 
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
 
+
     private var playlistId: Int? = null
     private var trackList: List<Track> = emptyList()
+
+    private var moveJob: Job? = null
+
+    private val trackAdapter: TrackShowBsAdapter = TrackShowBsAdapter() { track ->
+
+        if (moveJob != null && moveJob?.isActive == true) {
+            return@TrackShowBsAdapter
+        }
+        moveJob = lifecycleScope.launch {
+
+            AudioPlayerFragment.newInstance(track)
+            navigateToDestination(R.id.audioPlayerFragment)
+            delay(Constants.CLICK_DEBOUNCE_DELAY)
+//            dismiss()
+        }
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -53,18 +80,21 @@ class PlaylistInfoFragment : Fragment() {
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet!!)
         val minHeightBs = resources.getDimensionPixelSize(R.dimen.bottom_sheet_peek_height)
 
-
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerViewTrackSHow)
+        recyclerView.adapter = trackAdapter
 
         viewModel.getPlaylistWithTracks(playlistId!!)
         viewModel.playlistWithTracks.observe(viewLifecycleOwner) { playlistWithTracks ->
             setPlaylistInfo(playlistWithTracks)
             trackList = playlistWithTracks.playlistTracks
+            trackAdapter.updateTrackList(playlistWithTracks.playlistTracks)
 
-            if (trackList.isNotEmpty()){
+            if (trackList.isNotEmpty()) {
                 bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
                 bottomSheetBehavior.isHideable = false
                 bottomSheet.minimumHeight = minHeightBs
                 bottomSheetBehavior.peekHeight = minHeightBs
+
             } else {
                 bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
                 bottomSheet.isVisible = false
--- app/src/main/java/com/example/playlistmaker/media/presentation/playlist/TrackShowBsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media/presentation/playlist/TrackShowBsFragment.kt
@@ -4,19 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.findNavController
-import com.example.playlistmaker.R
-import com.example.playlistmaker.audio_player.presentation.AudioPlayerFragment
 import com.example.playlistmaker.databinding.FragmentTrackShowBsBinding
-import com.example.playlistmaker.media.presentation.playlist_adapter.TrackShowBsAdapter
-import com.example.playlistmaker.search.domain.models.Track
-import com.example.playlistmaker.utils.Constants
-import com.example.playlistmaker.utils.navigateToDestination
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class TrackShowBsFragment : BottomSheetDialogFragment() {
 
@@ -25,52 +14,56 @@ class TrackShowBsFragment : BottomSheetDialogFragment() {
         get() = _binding
             ?: throw IllegalStateException("Binding for FragmentTrackShowBsBinding must not be null")
 
-    private var trackList: List<Track> = emptyList()
-    private var moveJob: Job? = null
-
-    private val trackAdapter: TrackShowBsAdapter = TrackShowBsAdapter() { track ->
-
-        if (moveJob != null && moveJob?.isActive == true) {
-            return@TrackShowBsAdapter
-        }
-        moveJob = lifecycleScope.launch {
-
-            AudioPlayerFragment.newInstance(track)
-            navigateToDestination(R.id.audioPlayerFragment)
-            delay(Constants.CLICK_DEBOUNCE_DELAY)
-            dismiss()
-        }
-
-    }
+//    private var trackList: List<Track> = emptyList()
+//    private var moveJob: Job? = null
+//
+//    private val trackAdapter: TrackShowBsAdapter = TrackShowBsAdapter() { track ->
+//
+//        if (moveJob != null && moveJob?.isActive == true) {
+//            return@TrackShowBsAdapter
+//        }
+//        moveJob = lifecycleScope.launch {
+//
+//            AudioPlayerFragment.newInstance(track)
+//            navigateToDestination(R.id.audioPlayerFragment)
+//            delay(Constants.CLICK_DEBOUNCE_DELAY)
+//            dismiss()
+//        }
+//
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         _binding = FragmentTrackShowBsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        trackList = _trackListTemp ?: run {
-            view.findNavController().popBackStack()
-            return
-        }
+//        trackList = _trackListTemp ?: run {
+//            view.findNavController().popBackStack()
+//            return
+//        }
+
+//        binding.recyclerView.adapter = trackAdapter
+//        trackAdapter.updateTrackList(trackList)
 
 
     }
 
-    companion object {
-        private var _trackListTemp: List<Track>? = null
-
-        fun newInstance(trackList: List<Track>) = TrackShowBsFragment().apply {
-            _trackListTemp = trackList
-        }
-
-    }
+//    companion object {
+//        private var _trackListTemp: List<Track>? = null
+//
+//        fun newInstance(trackList: List<Track>) = TrackShowBsFragment().apply {
+//            _trackListTemp = trackList
+//        }
+//
+//    }
 
 
 }
